5e138667898db17094266aa6a43ae1b1b511e878
djrich87 <djrich87@gmail.com>
2022-02-02 21:22:25 -0700

    complete lab

--- js/app.js
+++ js/app.js
@@ -5,15 +5,11 @@ const btn = document.querySelector('button')
 
 btn.addEventListener('click', function(event){
     const toDoLi = document.createElement('Li')
-
     const input = document.querySelector('input')
-
     toDoLi.textContent = input.value
-
     document.querySelector('ul').appendChild(toDoLi)
     input.value = ""
     
     console.log(toDoLi)
-
 })
 
