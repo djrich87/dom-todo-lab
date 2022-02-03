6a310417c586ad883679301793ff95b61df8465c
djrich87 <djrich87@gmail.com>
2022-02-02 22:28:59 -0700

    complete lab

--- js/app.js
+++ js/app.js
@@ -1,16 +1,25 @@
-console.log('sanity check')
-
-
-
 const btn = document.querySelector('button')
 
 btn.addEventListener('click', function(event){
     const toDoLi = document.createElement('Li')
+
     const input = document.querySelector('input')
-    toDoLi.textContent = input.value
-    document.querySelector('ul').appendChild(toDoLi)
-    input.value = ""
+      toDoLi.textContent = input.value
     
+    if (input.value = ""){
+      document.querySelector('ul').appendChild(toDoLi)
+        input.value = ""
+    }
     console.log(toDoLi)
+
 })
 
+
+document.querySelector('ul').addEventListener('click', handleClick)
+
+function handleClick(evt){
+
+}
+
+// resetBtn.addEventListener('click', function(event) {
+// })
